cdd9b9e9e30edc42e318935e9f0ce1f2fa7a359a
surunzi <surunzi@foxmail.com>
2016-02-16 17:32:08 +0800

    Add: Eustia

--- src/renderer/Renderer.ts
+++ src/renderer/Renderer.ts
@@ -2,11 +2,12 @@ import FilterManager from './manager/FilterManager.ts'
 
 export default class Renderer
 {
+    filterManager:FilterManager;
     constructor(width:number, height:number, options:Object)
     {
         options = options || {};
 
         this.filterManager = new FilterManager();
     }
-    filterManager:FilterManager;
+    render() {}
 }
